fed9b055fa5224a80ffc77ec24b97f8e64cbc3ce
Precious Ebubechukwu <preciousada275@gmail.com>
2023-05-15 11:09:09 -0700

    Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+
+n that prints a matrix of integers"""
+
+
+def print_matrix_integer(matrix=[[]]):
+        for index in range(len(matrix)):
+                for i in range(len(matrix[index])):
+                            if i != 0:
+                                            print(" ", end='')
+                                                        print("{:d}".format(matrix[index][i]), end='')
+                                                                print()
