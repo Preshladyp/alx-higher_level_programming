959dd3fa55702bcb1b5130005ed5b0834ad209c1
Precious Ebubechukwu <preciousada275@gmail.com>
2023-05-15 11:52:58 -0700

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python3
 
-n that replaces an element in a list at a specific position without modifying the original list"""
+# 4-new_in_list.py
+
 
 
 def new_in_list(my_list, idx, element):
-        length = len(my_list)
 
-            copy_list = my_list[:]
+        """Replace an element in a copied list at a specific position."""
 
-                if 0 <= idx < length:
-                        copy_list[idx] = element
+            if idx < 0 or idx > (len(my_list) - 1):
 
-                            return(copy_list)
+                        return (my_list)
+
+
+                        copy = [x for x in my_list]
+
+                            copy[idx] = element
+
+                                return (copy)
