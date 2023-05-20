dc3503ad79887bd3fe1a14a618abb8967c0abe14
Precious Ebubechukwu <preciousada275@gmail.com>
2023-05-20 00:49:22 -0700

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+
 def simple_delete(a_dictionary, key=""):
-if a_dictionary.get(key) is not None:
-del a_dictionary[key]
-return (a_dictionary)
+    """
+    deletes an element based on the key from a dictionary
+    """
+    if key in a_dictionary:
+        a_dictionary.pop(key)
+    return (a_dictionary.copy())
