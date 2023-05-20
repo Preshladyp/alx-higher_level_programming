b3f606c8c420e79281006e25f343e7cb1b4b2c69
Precious Ebubechukwu <preciousada275@gmail.com>
2023-05-19 17:56:11 -0700

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+
 def multiply_by_2(a_dictionary):
-new_dir = a_dictionary.copy()
-list_keys = list(new_dir.keys())
-for i in list_keys:
-new_dir[i] *= 2
-return (new_dir)
+    """
+    Return a new dictionary with all values multiplied by 2
+    """
+    return {key: value*2 for (key, value) in a_dictionary.items()}
